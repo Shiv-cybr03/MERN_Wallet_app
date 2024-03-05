300c8d790a2ecea441ea6543ef91755455a2db03
Shiv yadav <yshiv4074@gmail.com>
2024-03-05 21:19:57 +0530

    create admin_user panel

--- client/src/components/DefaultLayout.js
+++ client/src/components/DefaultLayout.js
@@ -43,62 +43,60 @@ function DefaultLayout({ children }){
     ]
 
     const adminMenu = [
-        [
-            {
-                title: "Home",
-                icon: <i className="ri-home-7-line"></i>,
-                onClick : () => navigate("/"),
-                path: "/"
+        {
+            title: "Home",
+            icon: <i className="ri-home-7-line"></i>,
+            onClick : () => navigate("/"),
+            path: "/"
+        },
+        {
+            title: "Users",
+            icon: <i className="ri-user-settings-line"></i>,
+            onClick : () => navigate("/users"),
+            path: "/users"
+        },
+        {
+            title: "Transactions",
+            icon: <i className="ri-bank-line"></i>,
+            onClick : () => navigate("/transactions"),
+            path: "/transactions"
+        },
+        {
+            title: "Requests",
+            icon: <i className="ri-hand-heart-line"></i>,
+            onClick: () => navigate("/requests"),
+            path: "/requests"
+        },
+        {
+            title: "Profile",
+            icon: <i className="ri-user-3-line"></i>,
+            onClick: () => navigate("/profile"),
+            path: "/profile"
+        },
+        {
+            title: "Logout",
+            icon: <i className="ri-logout-box-line"></i>,
+            onClick: () => {
+                localStorage.removeItem("token");
+                navigate("/login");
             },
-            {
-                title: "users",
-                icon: <i className="ri-user-setting-line"></i>,
-                onClick : () => navigate("/users"),
-                path: "/"
-            },
-            {
-                title: "Transactions",
-                icon: <i className="ri-bank-line"></i>,
-                onClick : () => navigate("/transactions"),
-                path: "/transactions"
-            },
-            {
-                title: "Requests",
-                icon: <i className="ri-hand-heart-line"></i>,
-                onClick: () => navigate("/requests"),
-                path: "/requests"
-            },
-            {
-                title: "Profile",
-                icon: <i className="ri-user-3-line"></i>,
-                onClick: () => navigate("/profile"),
-                path: "/profile"
-            },
-            {
-                title: "Logout",
-                icon: <i className="ri-logout-box-line"></i>,
-                onClick: () => {
-                    localStorage.removeItem("token");
-                    navigate("/login");
-                },
-                path: "/logout"
-            }
-        ]
+            path: "/logout"
+        }
     ]
-    const menuToRender = user?.isAdmin ? adminMenu : userMenu;
+
+    const menuToRender = user?.isAdmin ? userMenu : adminMenu;
 
     return(
         <div className="layout">
             <div className="sidebar">
-                <div className="menu ">
-                    <h1>home</h1>
+                <div className="menu">
                     {menuToRender.map((item) =>{
                         const isActive = window.location.pathname === item.path;
                         return (
                             <div className={`menu-item ${isActive ? "active-menu-item" : ""}`} 
                             onClick={item.onClick}>
                             {item.icon}
-                            {!collapsed && ( <h1 className="text-white text-sm">{item.title}</h1>)}
+                            {!collapsed && ( <h1 className="text-sm">{item.title}</h1>)}
                         </div>
                         );
                     })}
@@ -106,19 +104,19 @@ function DefaultLayout({ children }){
             </div>
             <div className="body">
                 <div className="header flex justify-between items-center">
-                    <div className="text-white"> 
+                    <div className="text-secondary"> 
                     {collapsed && <i className="ri-close-line"
                         onClick={() => setCollapsed(!collapsed)}></i>}
                     {!collapsed && <i className="ri-menu-2-line"
                     onClick={() => setCollapsed(!collapsed)}></i>}
                     </div>
                     <div>
-                        <h1 className="text-xl text-white">
+                        <h1 className="text-xl text-secondary">
                             WALLET-WIN
                         </h1>
                     </div>
                     <div>
-                        <h1 className="text-sm underline text-white ">
+                        <h1 className="text-sm underline text-secondary">
                             {user?.firstName}-{user?.lastName    }
                         </h1>
                     </div>
